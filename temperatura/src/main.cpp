f712d342cce10a7cee1df779375f63c076f69120
RafaReyes <rafaelmreyes@hotmail.com>
2021-06-30 20:53:55 -0700

    Interupciones a boton encender apagar led

--- temperatura/src/main.cpp
+++ temperatura/src/main.cpp
@@ -2,29 +2,41 @@
 // declaracion de variables
 int led = 5;
 int btn = 3;
+bool estadoPulsador;
+//se debe de declarar todas las funciones de forma global
+void consulta_estado();
+
 void setup() {
   Serial.begin(9600);
 // declaracion de variables como in-out
-  pinMode(btn, INPUT);
+  pinMode(btn, INPUT_PULLUP);
   pinMode(led, OUTPUT);
+
+
+  // attachInterrupt es una funcion para activar interrupciones, podria recibir solo el nombre del pin, pero arduino recomienda que sea pasado con otra funcion, digitalPinToInterrupt, el cual recibe como parametro el pin, asi mismo atachInterrupt recibe 3 parametros, el pin, la funcion que se activara con ese pin, esta se escribe sin parentesis, y por ultimo cuando debe activarse la interrupcion, existen diferentes estados, RISING, FALLING, LOW, etc 
+  attachInterrupt(digitalPinToInterrupt(btn), consulta_estado, CHANGE);
 }
 
 void loop() {
-  //se declara el estado del boton leyendo el pin 3
-  bool estadoPulsador = digitalRead(3);
-// si el estado esta apagado
-  if(estadoPulsador == false){
+  
+  
+  
+}
+
+void consulta_estado(){
+ 
     // si el led esta encendido
    if(digitalRead(led) == true){
     //  apago el led
      digitalWrite(led,LOW);
+     Serial.println("Apagado");
   // le damos un delay para hacer antirebote
-  delay(400);
+     delay(400);
    } else {
     //  si el led esta apagado enciendo el led
     digitalWrite(led, HIGH);
+    Serial.println("Encendido");
     delay(400);
-  }
-
-}
-}
+      }   
+    }
+  
