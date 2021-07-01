f6dacf240e2cc20c92826db678eb08de1e2c0949
RafaReyes <rafaelmreyes@hotmail.com>
2021-06-30 19:58:55 -0700

    Encender apagar led con un solo boton

--- temperatura/src/main.cpp
+++ temperatura/src/main.cpp
@@ -1,25 +1,30 @@
 #include <Arduino.h>
-
+// declaracion de variables
 int led = 5;
 int btn = 3;
 void setup() {
   Serial.begin(9600);
-
+// declaracion de variables como in-out
   pinMode(btn, INPUT);
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  //se declara el estado del boton leyendo el pin 3
   bool estadoPulsador = digitalRead(3);
-  String estado;
-
-  if(estadoPulsador == true){
-   estado = "Apagado";
-   digitalWrite(5,LOW); 
-  } else {
-    estado = "Encendido";
-    digitalWrite(5,HIGH);
+// si el estado esta apagado
+  if(estadoPulsador == false){
+    // si el led esta encendido
+   if(digitalRead(led) == true){
+    //  apago el led
+     digitalWrite(led,LOW);
+  // le damos un delay para hacer antirebote
+  delay(400);
+   } else {
+    //  si el led esta apagado enciendo el led
+    digitalWrite(led, HIGH);
+    delay(400);
   }
-  Serial.println(estado);
-  delay(250);
+
+}
 }
